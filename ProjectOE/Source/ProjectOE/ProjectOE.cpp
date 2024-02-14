181a28e382bdd01a771f127fefa0b8cf6339dbfe
NewbrookT <devnewbrookt@gmail.com>
2024-02-14 14:32:10 +0000

    upload 5

---
+++ ProjectOE/Source/ProjectOE/ProjectOE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectOE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectOE, "ProjectOE" );
